8dd77c2e1fe1dcea00adfc061413f6b4bed9bd63
Allan MELIGNON <allan.melignon@gmail.com>
2021-03-09 19:44:35 +0100

    Add the secured auth for the backend

--- symfony/src/Entity/Administrator.php
+++ symfony/src/Entity/Administrator.php
@@ -2,24 +2,37 @@
 
 namespace App\Entity;
 
+use App\Repository\AdministratorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * Administrator
- *
- * @ORM\Table(name="administrator")
- * @ORM\Entity(repositoryClass="App\Repository\AdministratorRepository")
+ * @ORM\Entity(repositoryClass=AdministratorRepository::class)
  */
-class Administrator
+class Administrator implements UserInterface
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id_admin", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
      */
-    private $idAdmin;
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @var string|null
@@ -36,20 +49,6 @@ class Administrator
     private $firstname;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="email", type="string", length=255, nullable=true)
-     */
-    private $email;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="password", type="string", length=60, nullable=true, options={"fixed"=true})
-     */
-    private $password;
-
-    /**
      * @var \DateTime|null
      *
      * @ORM\Column(name="created", type="datetime", nullable=true)
@@ -59,6 +58,87 @@ class Administrator
     public function getIdAdmin(): ?int
     {
         return $this->idAdmin;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getName(): ?string
@@ -85,30 +165,6 @@ class Administrator
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(?string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
     public function getCreated(): ?\DateTimeInterface
     {
         return $this->created;
@@ -120,6 +176,4 @@ class Administrator
 
         return $this;
     }
-
-
 }
