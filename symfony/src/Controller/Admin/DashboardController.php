29d0079c1019cb76e0b282e46cdb80b4be2daa63
Allan MELIGNON <allan.melignon@gmail.com>
2021-04-16 16:14:36 +0200

    Link to switch to the front/back office

--- symfony/src/Controller/Admin/DashboardController.php
+++ symfony/src/Controller/Admin/DashboardController.php
@@ -48,11 +48,14 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section("Database");
         yield MenuItem::linkToCrud('Missions', 'fa fa-suitcase', Mission::class);
         yield MenuItem::linkToCrud('Missions types', 'fa fa-suitcase', MissionType::class);
         yield MenuItem::linkToCrud('Hideouts', 'fa fa-eye-slash', Hideout::class);
         yield MenuItem::linkToCrud('Hideout types', 'fa fa-eye-slash', HideoutType::class);
         yield MenuItem::linkToCrud('Specialities', 'fa fa-trophy', Speciality::class);
         yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
+        yield MenuItem::section("Link");
+        yield MenuItem::linkToRoute('To front office','fa fa-home', 'missions');
     }
 }
