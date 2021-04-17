12acd43f7c2ca1a96c0a985575cfe9798717f682
Allan MELIGNON <allan.melignon@gmail.com>
2021-04-17 18:09:12 +0200

    Form constraint : Hideout in the same country than the mission

--- symfony/src/Entity/Mission.php
+++ symfony/src/Entity/Mission.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Validator\MinAgent;
+use App\Validator\SameCountry;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -121,6 +122,7 @@ class Mission
      *     @ORM\JoinColumn(name="id_hideout", referencedColumnName="id_hideout")
      *   }
      * )
+     * @SameCountry()
      */
     private $idHideout;
 
